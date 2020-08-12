7ef8c14dd13c046653c72b9a57e0c04281e5850f
John Liu <liucong@yaehzee.com>
2020-08-12 20:06:28 +0800

    实现单链表的结构及增删操作

--- link-list/singleLinkList.ts
+++ link-list/singleLinkList.ts
@@ -1,8 +1,7 @@
 /*
  * @Author: John Liu
  * @Date: 2020-08-10 19:22:40
- * @LastEditTime: 2020-08-11 21:52:17
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-08-12 19:53:03
  * @Description: In User Settings Edit
  * @FilePath: \typescript-data-structure\link-list\main.ts
 */
@@ -10,29 +9,46 @@
 import SingleLinkListNode from './singleLinkListNode'
 
 
-interface LinkListAction<T> {
-     add: Function,
-     delete: Function,
-     find,
+interface LinkList<T> {
+     head: SingleLinkListNode<T>,
+     tail: SingleLinkListNode<T>,
+     add: (val: T) => SingleLinkList<T>,
+     delete: (val: T) => SingleLinkList<T>,
+     find: (val: T, callback: Function) => boolean,
 }
 
-class SingleLinkList<T> implements LinkListAction<T> {
-     head: SingleLinkListNode<T>
+class SingleLinkList<T> implements LinkList<T> {
+     public head: SingleLinkListNode<T>
+     public tail: SingleLinkListNode<T>
+     readonly length: number
      constructor() {
-          this.head.value = null
-          this.head.next = null
+          this.head = new SingleLinkListNode<T>(null, null)
+          this.tail = this.head
+          this.length = 1
      }
 
-     add(val: T) {
-
+     add(val: T): this {
+          const newNode = new SingleLinkListNode<T>(val, null)
+          this.tail.next = newNode
+          return this
      }
 
-     delete(val: T): T {
-          return val
+     delete(val: T): this {
+          let currentNode = this.head
+          let preNode = null
+          for(let i = 0; i < this.length; i++) {
+               if (currentNode.value === val) {
+                    preNode.next = currentNode.next
+                    return this
+               }
+               preNode =  currentNode
+               currentNode = currentNode.next
+          }
+          return this
      }
 
-     find({ val: T = undefined, callback: Function }) {
-
+     find(val: T, callback: Function) {
+          return false
      }
 
 }
--- link-list/singleLinkListNode.ts
+++ link-list/singleLinkListNode.ts
@@ -1,14 +1,14 @@
 /*
  * @Author: John Liu
  * @Date: 2020-08-11 21:00:58
- * @LastEditTime: 2020-08-11 21:06:27
+ * @LastEditTime: 2020-08-12 18:45:27
  * @LastEditors: Please set LastEditors
  * @Description: Node of single linkList 
  * @FilePath: \typescript-data-structure\link-list\singleLinkListNode.ts
  */
 class SingleLinkListNode<T> {
-  value: T
-  next: any
+  value: T | null
+  next: SingleLinkListNode<T>
   constructor(value, next = null) {
     this.value = value
     this.next = next
